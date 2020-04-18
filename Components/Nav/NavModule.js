2e26293cec67ac0cbd95e195718386aa84ceb6a6
Berin <berin2@pdx.edu>
2020-04-18 15:18:45 -0700

    I added some comments for clarification!

--- Components/Nav/NavModule.js
+++ Components/Nav/NavModule.js
@@ -1,27 +1,32 @@
 import "./NavModuleStyles.css";
 import React from 'react';
 
+//This component returns rendered contents for a contracted nav-drawer.
 function RetractedNavContents(props){
     return (<p>placeholder</p>);
 }
-//This component renders nav links in the  extended nav drawer.
+//This component returns rendered contents for an extended nav-drawer.
 function ExtendedNavContents(props){
     const linkArray = props.links.map((link) => <li className="nav-link"> {link.title} </li>)
 
     return (<ul>{linkArray}</ul>);
+
 }
-//This function returns the navbar component.
+
+
+//This class returns the navbar component.
 
 class NavDrawer extends React.Component {
     
     constructor(props){
         super(props)
+        //extended in state is to communicate whether or not the nav drawer is extended (set to true),
+        // retracted (set to false)!
         this.state = {extended: true, links: props.links}
     }
 
-    
-    
     render (){
+      //This line determines what to render in the Nav body based upon the states extended boolean value.
       var nav_contents = this.state.extended ? <ExtendedNavContents links={this.state.links}/> : <RetractedNavContents/>
     
       return (
